437230bc899d44b12f5f9cfee51767fd5c8a6d32
Reza <mohashakibaei@outlook.fr>
2021-05-14 10:01:58 +0200

    no message

--- cra-infocom/cra-view-2020/src/app/app-routing.module.ts
+++ cra-infocom/cra-view-2020/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddCollaborateurComponent } from './administrateur/add-collaborateur/add-collaborateur.component'
+import { AdministrateurComponent } from './administrateur/administrateur.component';
 import { DeleteCollaborateurComponent } from './administrateur/delete-collaborateur/delete-collaborateur.component'
 import { EditCollaborateurComponent} from './administrateur/edit-collaborateur/edit-collaborateur.component'
 import { ListCollaborateurComponent } from './administrateur/list-collaborateur/list-collaborateur.component';
@@ -12,10 +13,11 @@ const routes: Routes = [
   {path:'',component:ConnexionComponent },
   {path:'connexion',component:ConnexionComponent },
 
+  {path:'administrateur',component:AdministrateurComponent },
   {path:'addCollaborateur',component:AddCollaborateurComponent },
   {path:'deleteCollaborateur',component: DeleteCollaborateurComponent},
   {path:'editCollaborateur',component: EditCollaborateurComponent},
-  {path:'listCollaborateur',component:ListCollaborateurComponent },
+  {path:'administrateur/listCollaborateur',component:ListCollaborateurComponent },
 
 ];
 
