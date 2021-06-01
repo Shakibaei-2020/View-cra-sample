1b890a8dea84dafb763078486b1085b667b5fe71
Reza <mohashakibaei@outlook.fr>
2021-06-01 22:49:39 +0200

    no message

--- cra-infocom/cra-view-2020/src/app/utilisateur/declaration-activite/declaration-activite.component.ts
+++ cra-infocom/cra-view-2020/src/app/utilisateur/declaration-activite/declaration-activite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SystemJsNgModuleLoader } from '@angular/core';
 
 @Component({
   selector: 'app-declaration-activite',
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeclarationActiviteComponent implements OnInit {
 
-  constructor() { }
+dt = new Date();
+
+month!: number;
+year!: number;
+daysInMonth!: number;
+tabJours=new Array();
+
+calendar = document.querySelector("app-calendar")
+
+
+nosDates!: number[];
+
+
+
+  constructor() { 
+
+  
+  }
+
+  
+
 
   ngOnInit(): void {
+
+
+
+    this.month = this.dt.getMonth()+1;
+    this.year = this.dt.getFullYear();
+
+   this.daysInMonth = new Date(this.year, this.month, 0).getDate();
+    console.log(this.daysInMonth);
+
+
+    var i;
+    for (i = 0; i < this.daysInMonth; i++) {
+      this.tabJours[i] = i + 1;
+    }
+
+
+
   }
+
+  
 
 }
